7ba9250df69e8686572b1ab5d542bac3acf69ccd
Chris Wilton <cwilton@scottlogic.co.uk>
2020-02-06 09:55:37 +0000

    internal: fix switch loader for Windows paths

--- packages/perspective-webpack-plugin/src/js/switch-inline-loader.js
+++ packages/perspective-webpack-plugin/src/js/switch-inline-loader.js
@@ -6,13 +6,24 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
+const path = require("path");
 
 exports.default = function pitch(request) {
     return request;
 };
 
 exports.pitch = function(request) {
-    return `module.exports = require("${request.replace("umd/perspective.inline", "esm/perspective.parallel")}");`;
+    const parsedModulePath =
+        (process.platform.startsWith('win')
+            ? request.replace(/\\/g, "\\\\")
+            : request
+        )
+        .replace(
+            path.join("umd", "perspective.inline"),
+            path.join("esm", "perspective.parallel")
+        );
+
+    return `module.exports = require("${parsedModulePath}")`;
 };
 
 exports.raw = true;
